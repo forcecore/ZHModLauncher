0920a0943657633605722a566ca8bebb13a0a70b
ssanzing <ssanzing@xpbox>
2009-07-05 18:32:25 +0900

    Interface Designed

--- src/mainform.pas
+++ src/mainform.pas
@@ -5,10 +5,21 @@ unit MainForm;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  StdCtrls, ExtCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    BtnRun: TButton;
+    BtnRunWin: TButton;
+    BtnMod: TButton;
+    BtnRestore: TButton;
+    ComboBox1: TComboBox;
+    ImgLogo: TImage;
+    procedure ImgLogoClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -19,6 +30,13 @@ var
   Form1: TForm1; 
 
 implementation
+
+{ TForm1 }
+
+procedure TForm1.ImgLogoClick(Sender: TObject);
+begin
+  // we will open red2.net URL.
+end;
 
 initialization
   {$I mainform.lrs}
