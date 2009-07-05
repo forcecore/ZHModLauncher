07323a0edd27185e40d853e96cd8d403880c58df
ssanzing <ssanzing@xpbox>
2009-07-05 20:00:20 +0900

    now able to get "my documents"

--- src/mainform.pas
+++ src/mainform.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, ExtCtrls;
+  StdCtrls, ExtCtrls, Windows;
 
 type
 
@@ -17,12 +17,16 @@ type
     BtnRunWin: TButton;
     BtnMod: TButton;
     BtnRestore: TButton;
-    ComboBox1: TComboBox;
+    ModList: TComboBox;
     ImgLogo: TImage;
     procedure FormCreate(Sender: TObject);
     procedure ImgLogoClick(Sender: TObject);
   private
     { private declarations }
+    // jdj's own procedures
+    procedure LoadLogo();
+    procedure ScanMod();
+    function GetMyDoc(): string;
   public
     { public declarations }
   end; 
@@ -39,13 +43,42 @@ begin
   // we will open red2.net URL.
 end;
 
+// Load Logo Image
+procedure TFormMain.LoadLogo();
+begin
+  // we need to this, to properly use the transparency.
+  ImgLogo.Transparent:= true;
+  ImgLogo.Picture.PNG.LoadFromFile('logo.png');
+end;
+
+// Scan mod directory.
+procedure TFormMain.ScanMod();
+var
+  mydoc: string;
+begin
+  mydoc := GetMyDoc();
+end;
+
+// get mydocuments
+function TFormMain.GetMyDoc(): string;
+var
+  PIDL : PItemIDList;
+  Folder : array[0..MAX_PATH] of Char;
+const
+  // CSIDL_APPDATA  = $001A;
+  CSIDL_PERSONAL = $0005;
+begin
+  SHGetSpecialFolderLocation(0, CSIDL_PERSONAL, PIDL);
+  SHGetPathFromIDList(PIDL, Folder);
+  result:=Folder;
+end;
+
 // Initialization of the program (?),
 // at least this form.
 procedure TFormMain.FormCreate(Sender: TObject);
 begin
-  // Load Logo Image
-  ImgLogo.Transparent:= true;
-  ImgLogo.Picture.PNG.LoadFromFile('logo.png');
+  LoadLogo();
+  ScanMod();
 end;
 
 initialization
