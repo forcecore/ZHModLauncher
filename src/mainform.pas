b5a8f7129c2ac26470dd2f6eacf22039cc724eb5
forcecore <ssanzing@gmail.com>
2016-04-25 01:06:16 +0900

    zbig method implemented. No more -mod method.

--- src/mainform.pas
+++ src/mainform.pas
@@ -8,7 +8,7 @@ uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
   StdCtrls, ExtCtrls,
   // my additions
-  Windows, Registry, Dos;
+  Windows, Registry, Process;
 
 type
 
@@ -25,6 +25,7 @@ type
     procedure BtnRestoreClick(Sender: TObject);
     procedure BtnRunClick(Sender: TObject);
     procedure BtnRunWinClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure ImgLogoClick(Sender: TObject);
   private
@@ -32,15 +33,26 @@ type
     // private variable for convenience
     script: string;
     dscript: string;
+    game_dir: string;
     game_exe: string;
-    moddir: string;
+    //moddir: string;
+
+    // Moved files for running mod
+    zbigs: TStringList; // remembers what files are moved to game dir.
+    active_mod: string; // remembers what mod is active now.
+
     // jdj's own procedures
-    procedure ScanMod();
+    procedure ScanMods();
     procedure GetDirs();
-    function GetMyDoc(): string;
+    //function GetMyDoc(): string;
     function GenReg( t: string ): string;
     procedure Warning( t: string );
     procedure ScanScripts();
+    procedure LaunchGame( mod_name:string; params: string );
+
+    procedure ActivateMod( mod_name: string );
+    procedure DeactivateMod( mod_name: string );
+
   public
     { public declarations }
   end; 
@@ -80,19 +92,23 @@ begin
 end;
 
 // Scan mod directory.
-procedure TFormMain.ScanMod();
+procedure TFormMain.ScanMods();
 var
-  Dir : SearchRec;
+  info : TSearchRec;
+  mod_dir : string;
 begin
-  // scan the list
-  FindFirst(IncludeTrailingPathDelimiter(moddir)+'*.big', archive, Dir);
-  while (DosError=0) do
-   begin
-     ModList.Items.Add( Dir.Name );
-     //Writeln(Dir.Name+Space(40-Length(Dir.Name)),Dir.Size:9);
-     FindNext(Dir);
-   end;
-  FindClose(Dir);
+  // scan the dirs in gamedir/Mods
+  mod_dir := IncludeTrailingPathDelimiter(game_dir)+'Mods';
+  If FindFirst( mod_dir + '\*', faAnyFile and faDirectory, info ) = 0 then
+    begin
+    Repeat
+      if (info.Name <> '.') AND (info.Name <> '..') then
+        begin
+          ModList.Items.Add( info.Name );
+        end;
+    Until FindNext(info) <> 0;
+    end;
+  SysUtils.FindClose( info );
 
   if( ModList.Items.Count = 0 ) then
     Warning( '발견된 모드가 없습니다.' )
@@ -124,19 +140,27 @@ end;
 // get directories we need for the launcher.
 procedure TFormMain.GetDirs();
 var
-  mydoc: string;
-  dataleaf: string;
+  //mydoc: string;
+  //dataleaf: string;
   installdir: string;
 begin
-  mydoc := GetMyDoc();
-  dataleaf := GenReg( 'UserDataLeafName' );
+  //mydoc := GetMyDoc();
+  //dataleaf := GenReg( 'UserDataLeafName' );
   installdir := GenReg( 'InstallPath' );
+  if( FileExists( installdir ) ) then
+    begin
+      // installdir might contain Generals.exe!
+      // If file, FileExists returns true.
+      // If dir, returns False.
+      installdir := ExtractFilePath( installdir );
+    end;
 
   // we are ready to calculate now.
-  mydoc := IncludeTrailingPathDelimiter( mydoc );
-  moddir := mydoc + dataleaf;
+  //mydoc := IncludeTrailingPathDelimiter( mydoc );
+  //moddir := mydoc + dataleaf;
 
   installdir := IncludeTrailingPathDelimiter( installdir );
+  game_dir := installdir;
   game_exe := installdir + 'generals.exe';
   script := installdir + 'Data\Scripts';
   dscript := installdir + 'Data\_Scripts';
@@ -148,44 +172,118 @@ begin
 end;
 
 // get mydocuments
-function TFormMain.GetMyDoc(): string;
-var
-  PIDL : PItemIDList;
-  Folder : array[0..MAX_PATH] of Char;
-const
-  // CSIDL_APPDATA  = $001A;
-  CSIDL_PERSONAL = $0005;
-begin
-  SHGetSpecialFolderLocation(0, CSIDL_PERSONAL, PIDL);
-  SHGetPathFromIDList(PIDL, Folder);
-  result:=Folder;
-end;
+//function TFormMain.GetMyDoc(): string;
+//var
+//  PIDL : PItemIDList;
+//  Folder : array[0..MAX_PATH] of Char;
+//const
+//  // CSIDL_APPDATA  = $001A;
+//  CSIDL_PERSONAL = $0005;
+//begin
+//  SHGetSpecialFolderLocation(0, CSIDL_PERSONAL, PIDL);
+//  SHGetPathFromIDList(PIDL, Folder);
+//  result:=Folder;
+//end;
 
 // Initialization of the program (?),
 // at least this form.
 procedure TFormMain.FormCreate(Sender: TObject);
 begin
+  zbigs := TStringList.create;
   GetDirs();
-  ScanMod();
+  ScanMods();
   ScanScripts();
 end;
 
-procedure TFormMain.BtnRunClick(Sender: TObject);
+// Launch the game and wait for it to finish.
+procedure TFormMain.LaunchGame( mod_name: string; params: string );
 var
-  param: string;
+  output: string;
 begin
-  // run the game with -mod parm.
-  param := '-mod ' + '"' + ModList.Text + '"';
-  ShellExecute( 0, 'open', PChar(game_exe), PChar(param), nil, SW_NORMAL );
+  // Set the mod as active
+  ActivateMod( mod_name );
+
+  // Run the mod
+  //ShellExecute( 0, 'open', PChar(game_exe), PChar(params), nil, SW_NORMAL );
+  output := '';
+  RunCommand( game_exe, params, output );
+
+  // Wait for it to finish
+  DeactivateMod( mod_name );
+end;
+
+procedure TFormMain.ActivateMod( mod_name: string );
+var
+  i : Integer;
+  info : TSearchRec;
+  fname, mod_path: string;
+  src, dest: string;
+begin
+  active_mod := ModList.Text;
+
+  // Find zbigs in the game/Mods/mod_name then move it to game dir.
+  mod_path := IncludeTrailingPathDelimiter(game_dir) +
+    'Mods\' + mod_name + '\';
+
+  If FindFirst( mod_path + '*.zbig', faAnyFile, info ) = 0 then
+    begin
+    Repeat
+      // Add them to the mod zbig files list.
+      zbigs.Add( info.name );
+    Until FindNext(info) <> 0;
+    end;
+  SysUtils.FindClose( info );
+
+  // For each items in zbigs, move the file to the game dir.
+  for i := 0 to zbigs.Count-1 do
+  begin
+    fname := zbigs[ i ];
+    src := mod_path + fname;
+    dest := game_dir + fname;
+    dest := StringReplace( dest, '.zbig', '.big', [rfReplaceAll] );
+    MoveFile( PChar(src), PChar(dest) );
+  end;
+end;
+
+procedure TFormMain.DeactivateMod( mod_name: string );
+var
+  i : Integer;
+  fname, mod_path: string;
+  src, dest: string;
+begin
+  active_mod := '';
+
+  // Find zbigs in the game/Mods/mod_name then move it to game dir.
+  mod_path := IncludeTrailingPathDelimiter(game_dir) +
+    'Mods\' + mod_name + '\';
+
+    // For each items in zbigs, move the file to the game dir.
+  for i := 0 to zbigs.Count-1 do
+  begin
+    fname := zbigs[ i ];
+    dest := mod_path + fname;
+    src := game_dir + fname;
+    src := StringReplace( src, '.zbig', '.big', [rfReplaceAll] );
+    //Warning( src + ' --> ' + dest );
+    MoveFile( PChar(src), PChar(dest) );
+  end;
+
+  zbigs.clear;
+end;
+
+procedure TFormMain.BtnRunClick(Sender: TObject);
+begin
+  LaunchGame( ModList.Text, '' );
 end;
 
 procedure TFormMain.BtnRunWinClick(Sender: TObject);
-var
-  param: string;
 begin
-  // run the game with -win -mod parm.
-  param := '-win -mod ' + '"' + ModList.Text + '"';
-  ShellExecute( 0, 'open', PChar(game_exe), PChar(param), nil, SW_NORMAL );
+  LaunchGame( ModList.Text, '-win' );
+end;
+
+procedure TFormMain.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  zbigs.free;
 end;
 
 procedure TFormMain.BtnModClick(Sender: TObject);
